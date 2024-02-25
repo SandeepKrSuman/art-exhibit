c133efbb0c1ba537db8669a994bd9c95c4771fc7
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2024-02-25 14:42:37 +0530

    Login Component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import styles from "./Login.module.css";
+
+export default function Login() {
+  const [newac, setNewac] = useState(false);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (event) => {
+    console.log("Sign Up");
+    event.preventDefault();
+  };
+
+  const handleSignIn = (event) => {
+    console.log("Sign In");
+    event.preventDefault();
+  };
+
+  return (
+    <section className={styles.login}>
+      <h1>{newac ? "Create Account" : "Login"}</h1>
+      <form
+        className={styles.details}
+        onSubmit={newac ? handleSignUp : handleSignIn}
+      >
+        {newac && (
+          <>
+            <label htmlFor="fname">First Name:</label>
+            <input
+              id="fname"
+              name="fname"
+              type="text"
+              required
+              value={fname}
+              onChange={(e) => setFname(e.target.value)}
+            />
+            <label htmlFor="lname">Last Name:</label>
+            <input
+              id="lname"
+              name="lname"
+              type="text"
+              required
+              value={lname}
+              onChange={(e) => setLname(e.target.value)}
+            />
+          </>
+        )}
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="email">Password:</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>{newac ? "Sign Up" : "Sign In"}</button>
+        <span onClick={() => setNewac((prev) => !prev)}>
+          {newac
+            ? "Already have an account? Sign In"
+            : "Don't have an account? Sign Up"}
+        </span>
+      </form>
+    </section>
+  );
+}
