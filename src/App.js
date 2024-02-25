d228f95d6add555ae96530654b1724aa130d091d
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2024-02-25 14:42:58 +0530

    handle routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Contact from "./components/Contact/Contact";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Page404 from "./components/Page404/Page404";
+import Bag from "./components/Bag/Bag";
+import Profile from "./components/Profile/Profile";
+import Login from "./components/Login/Login";
 
 export default function App() {
   return (
@@ -18,6 +21,9 @@ export default function App() {
           <Route path="/about" exact element={<About />} />
           <Route path="/collections" exact element={<Collections />} />
           <Route path="/contact" exact element={<Contact />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/bag" exact element={<Bag />} />
+          <Route path="/profile" exact element={<Profile />} />
           <Route path="/*" element={<Page404 />} />
         </Routes>
       </Router>
